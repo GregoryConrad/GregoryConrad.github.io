ab52eaab3a745915a1f393c1e8e4f6d3cb389a1d
Gregory Conrad <gregorysconrad@gmail.com>
2019-10-01 09:41:42 -0400

    Updated mirror

--- downloads/drivemaker.bash
+++ downloads/drivemaker.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Simple Bootable USB Drive Maker for Mac
-#Author: ETCG (Epic Tin Can Games)
+#Author: Gregory Conrad
 
 #Under MIT License:
 
-#Copyright (c) 2016 ETCG
+#Copyright (c) 2016 Gregory Conrad
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -55,7 +55,7 @@ if [ "$userOption" == "A" ]; then
 	fi
 elif [ "$userOption" == "B" ]; then
 	echo "Downloading Debian..."
-	if ! downloadFile "https://caesar.ftp.acc.umu.se/debian-cd/current/amd64/iso-cd/debian-"$DEBIAN_VERSION"-amd64-netinst.iso"
+	if ! downloadFile "https://gemmei.ftp.acc.umu.se/debian-cd/current/amd64/iso-cd/debian-"$DEBIAN_VERSION"-amd64-netinst.iso"
 	then
 		exit 1
 	fi
